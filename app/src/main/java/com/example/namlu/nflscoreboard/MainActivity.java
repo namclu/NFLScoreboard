f5f0129b7761c45c746f91e4bdc36e65e44deefe
namclu <namlu1205@gmail.com>
2017-01-05 20:22:49 +1100

    2 implement setOnClickListener on button_touchdown to display 6 when tapped. update AndroidManifest.xml to add MainActivity.java as launcher activity

--- app/src/main/java/com/example/namlu/nflscoreboard/MainActivity.java
+++ app/src/main/java/com/example/namlu/nflscoreboard/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.namlu.nflscoreboard;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button touchdownButton = (Button) findViewById(R.id.button_touchdown);
+        touchdownButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                displayForTeamA(6);
+            }
+        });
+    }
+
+    public void displayForTeamA(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_a_score);
+        scoreView.setText(String.valueOf(score));
     }
 }

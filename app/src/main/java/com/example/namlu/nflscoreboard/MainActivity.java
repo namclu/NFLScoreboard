f40aaade74d12d72738e10cf2e80114f056b1785
namclu <namlu1205@gmail.com>
2017-01-07 16:58:48 +1100

    2 add comments to button when they are clicked in MainActivity.java

--- app/src/main/java/com/example/namlu/nflscoreboard/MainActivity.java
+++ app/src/main/java/com/example/namlu/nflscoreboard/MainActivity.java
@@ -20,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Add 6 points for touchdown when button is clicked
         touchdownButton = (Button) findViewById(R.id.button_touchdown);
         touchdownButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -28,6 +29,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        // Add 3 points for field goal when button is clicked
         fieldGoalButton = (Button) findViewById(R.id.button_fieldgoal);
         fieldGoalButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -36,6 +38,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        // Add 2 points for two point conversion when button is clicked
         twoPointConversionButton = (Button) findViewById(R.id.button_two_point_conversion);
         twoPointConversionButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -44,6 +47,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        // Add 1 point for extra point when button is clicked
         extraPointButton = (Button) findViewById(R.id.button_extra_point);
         extraPointButton.setOnClickListener(new View.OnClickListener() {
             @Override

a6ce6c411143580f5978a03f2dacba537391927f
namclu <namlu1205@gmail.com>
2017-01-07 18:41:39 +1100

    4 add comments, add references for team B buttons, add OnClickListener() to team B buttons, test and confirm score on team B button implementions

--- app/src/main/java/com/example/namlu/nflscoreboard/MainActivity.java
+++ app/src/main/java/com/example/namlu/nflscoreboard/MainActivity.java
@@ -8,7 +8,10 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    // Global variables
+    /*
+     * @param scoreTeamA tracks the cumulative score for team A
+     * @param scoreTeamB tracks the cumulative score for team B
+     */
     int scoreTeamA = 0;
     int scoreTeamB = 0;
 
@@ -24,8 +27,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        /*
+         * Code interaction for Team A
+         */
         // Add 6 points for touchdown when button is clicked
-        touchdownButton = (Button) findViewById(R.id.button_touchdown);
+        touchdownButton = (Button) findViewById(R.id.button_touchdown_a);
         touchdownButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -63,10 +69,60 @@ public class MainActivity extends AppCompatActivity {
                 displayForTeamA(scoreTeamA);
             }
         });
+
+        /*
+         * Code interaction for Team B
+         */
+        // Add 6 points for touchdown when button is clicked
+        touchdownButton = (Button) findViewById(R.id.button_touchdown_b);
+        touchdownButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                scoreTeamB += 6;
+                displayForTeamB(scoreTeamB);
+            }
+        });
+
+        // Add 3 points for field goal when button is clicked
+        fieldGoalButton = (Button) findViewById(R.id.button_field_goal_b);
+        fieldGoalButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                scoreTeamB += 3;
+                displayForTeamB(scoreTeamB);
+            }
+        });
+
+        // Add 2 points for two point conversion when button is clicked
+        twoPointConversionButton = (Button) findViewById(R.id.button_two_point_conversion_b);
+        twoPointConversionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                scoreTeamB += 2;
+                displayForTeamB(scoreTeamB);
+            }
+        });
+
+        // Add 1 point for extra point when button is clicked
+        extraPointButton = (Button) findViewById(R.id.button_extra_point_b);
+        extraPointButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                scoreTeamB += 1;
+                displayForTeamB(scoreTeamB);
+            }
+        });
     }
 
+    // displayForTeamA() sets the score for team A
     public void displayForTeamA(int score) {
-        TextView scoreView = (TextView) findViewById(R.id.team_a_score);
+        TextView scoreView = (TextView) findViewById(R.id.score_team_a);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    // displayForTeamB() sets the score for team B
+    public void displayForTeamB(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.score_team_b);
         scoreView.setText(String.valueOf(score));
     }
 }
